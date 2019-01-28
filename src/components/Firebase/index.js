b408d71f249e9ae9a185c442fa176946c95be9ab
Moitrayee Choudhury <moitrayee3009@gmail.com>
2019-01-28 02:08:35 +0100

    fix error

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,9 +1,9 @@
-import FirebaseContext, { withFirebase } from './context';
+import FirebaseContext from './context';
 import Firebase from './firebase';
 
 export default Firebase;
 
-export { FirebaseContext, withFirebase };
+export { FirebaseContext};
 
 
 
